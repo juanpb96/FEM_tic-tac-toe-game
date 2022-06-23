8d817a1410b51927785b9d3a2f6690c710bef024
MasterDev <juanpb.96@gmail.com>
2022-06-23 18:44:46 -0500

    Add context and app router

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,38 +1,53 @@
-export const TicTacToe = () => {
-    return (
-        <>
-            {/* <!-- New game menu start --> */}
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { PlayerCoxtext } from './hocs/PlayerContext';
+import { AppRouter } from './routers/AppRouter';
 
-            Pick player 1's mark
-            Remember: X goes first
-        
-            New Game (vs CPU)
-            New Game (vs player)
-        
-            {/* <!-- New game menu end --> */}
-        
-            {/* <!-- Game board start --> */}
-        
-            {/* <!-- x/o icon --> turn */}
-        
-            {/* X (You) <!-- Your score --> */}
-            {/* Ties <!-- Ties score --> */}
-            {/* X (CPU) <!-- CPU score --> */}
-        
-            Oh no, you lost
-            You won!
-            {/* Player <!-- 1/2 --> wins! */}
-        
-            {/* <!-- x/o icon --> takes the round */}
-            Round tied
-            Restart game?
-        
-            Quit
-            Next round
-            No, cancel
-            Yes, restart
-        
-            {/* <!-- Game board end --> */}
-        </>
+export const TicTacToe = () => {
+    const [player, setPlayer] = useState('X');
+
+    return (
+        <PlayerCoxtext.Provider value={{
+            player,
+            setPlayer
+        }}>
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
+        </PlayerCoxtext.Provider>
     );
 };
+
+
+//  {/* <!-- New game menu start --> */}
+
+//  Pick player 1's mark
+//  Remember: X goes first
+
+//  New Game (vs CPU)
+//  New Game (vs player)
+
+//  {/* <!-- New game menu end --> */}
+
+//  {/* <!-- Game board start --> */}
+
+//  {/* <!-- x/o icon --> turn */}
+
+//  {/* X (You) <!-- Your score --> */}
+//  {/* Ties <!-- Ties score --> */}
+//  {/* X (CPU) <!-- CPU score --> */}
+
+//  Oh no, you lost
+//  You won!
+//  {/* Player <!-- 1/2 --> wins! */}
+
+//  {/* <!-- x/o icon --> takes the round */}
+//  Round tied
+//  Restart game?
+
+//  Quit
+//  Next round
+//  No, cancel
+//  Yes, restart
+
+//  {/* <!-- Game board end --> */}
