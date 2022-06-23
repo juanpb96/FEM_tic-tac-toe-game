f68ee213a23f870580a16cff9477fea18ffa456c
MasterDev <juanpb.96@gmail.com>
2022-06-23 18:51:15 -0500

    Switch player mark when turn changes

--- src/components/Game/GameHeader.js
+++ src/components/Game/GameHeader.js
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
+import { ASSETS_PATH } from '../../helpers/constants';
+import { PlayerCoxtext } from '../../hocs/PlayerContext';
+
 export const GameHeader = () => {
+    const { player } = useContext(PlayerCoxtext);
+
     return (
         <header>
             <h1>Tic tac toe</h1>
-            <img src={`${process.env.PUBLIC_URL}/assets/logo.svg`} alt='' />
-            <p>TURN</p>
+            <img src={`${ASSETS_PATH}/logo.svg`} alt='' />
+            <section>
+                <img src={`${ASSETS_PATH}/icon-turn-${player}.svg`} alt={ player } />
+                <h2>TURN</h2>
+            </section>
             <button aria-label='restart'>
-                <img src={`${process.env.PUBLIC_URL}/assets/icon-restart.svg`} alt='' />
+                <img src={`${ASSETS_PATH}/icon-restart.svg`} alt='' />
             </button>
         </header>
     );
--- src/components/Game/tests/GameHeader.test.js
+++ src/components/Game/tests/GameHeader.test.js
@@ -1,14 +1,37 @@
 import { render, screen } from "@testing-library/react";
+import { PlayerCoxtext } from "../../../hocs/PlayerContext";
 import { GameHeader } from "../GameHeader";
 
 describe('Test <GameHeader />', () => { 
     test('should render elements correctly', () => { 
-        render(<GameHeader />);
+        render(
+            <PlayerCoxtext.Provider value={{ player: 'X' }}>
+                <GameHeader />
+            </PlayerCoxtext.Provider>
+        );
 
         expect(screen.getByRole('heading', { name: 'Tic tac toe' })).toBeInTheDocument();
         expect(screen.getAllByRole('img')[0].getAttribute('src').includes('logo')).toBeTruthy();
         expect(screen.getByText('TURN')).toBeInTheDocument();
         expect(screen.getByLabelText('restart')).toBeInTheDocument();
-        expect(screen.getAllByRole('img')[1].getAttribute('src').includes('restart')).toBeTruthy();
-    });   
+        expect(screen.getByLabelText('restart').children.item(0).src.includes('restart')).toBeTruthy();
+    });
+
+    test('should switch player turn', () => { 
+        const { rerender } = render(
+            <PlayerCoxtext.Provider value={{ player: 'X' }}>
+                <GameHeader />
+            </PlayerCoxtext.Provider>
+        );
+
+        expect(screen.getByAltText('X')).toBeInTheDocument();
+        
+        rerender(
+            <PlayerCoxtext.Provider value={{ player: 'O' }}>
+                <GameHeader />
+            </PlayerCoxtext.Provider>
+        );
+        
+        expect(screen.getByAltText('O')).toBeInTheDocument();
+    });
 });
