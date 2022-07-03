c18da9e6654642aa84474d236b4404c205d248b3
MasterDev <juanpb.96@gmail.com>
2022-07-02 20:09:01 -0500

    Add localStorage mock

---
+++ src/helpers/mocks/localStorage.mock.js
@@ -0,0 +1,17 @@
+export const localStorageMock = (function() {
+    let store = {};
+    
+    return {
+        getItem(key) {
+            return store[key];
+        },
+        setItem(key, value) {
+            store[key] = value;
+        },
+        clear() {
+            store = {};
+        },
+    };
+})();
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
