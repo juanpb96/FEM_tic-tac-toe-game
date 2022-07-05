c494103322dea0871606ad9202553c4526b5366a
MasterDev <juanpb.96@gmail.com>
2022-07-04 21:54:38 -0500

    Validate when user wants to restart a game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react';
 import { GameBoard } from './GameBoard';
 import { GameFooter } from './GameFooter';
 import { GameHeader } from './GameHeader';
+import { GameModal } from './GameModal';
+
+const modalInitialState = {
+    type: '',
+    winnerMark: ''
+};
 
 export const Game = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [modalValues, setModalValues] = useState(modalInitialState);
+
+    useEffect(() => {
+        if (!showModal) {
+            setModalValues(modalInitialState);
+        }
+    }, [showModal]);
+
+    const { type, winnerMark } = modalValues;
+    
     return (
         <>
-            <GameHeader />
-            <GameBoard />
+            <GameHeader
+                setModalValues={ setModalValues }
+                setShowModal={ setShowModal } />
+            {
+                showModal &&
+                <GameModal
+                    type={ type }
+                    winnerMark={ winnerMark }
+                    setShowModal={ setShowModal }
+                />
+            }
+            <GameBoard
+                setModalValues={ setModalValues }
+                setShowModal={ setShowModal }
+            />
             <GameFooter />
         </>
     );
--- src/components/Game/tests/Game.test.js
+++ src/components/Game/tests/Game.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import { PlayerCoxtext } from "../../../hocs/PlayerContext";
-import { Game } from "../Game";
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
+import { PlayerCoxtext } from '../../../hocs/PlayerContext';
+import { Game } from '../Game';
 
 const [player, setPlayer] = ['X', jest.fn()];
 
-describe('Test <GameStart />', () => { 
-    beforeEach(() => {
+describe('Test <GameStart />', () => {
+    test('should contain main title', () => { 
         render(
             <PlayerCoxtext.Provider value={{
                 player,
@@ -14,11 +14,34 @@ describe('Test <GameStart />', () => {
                 <Game />
             </PlayerCoxtext.Provider>
         );
-    });
 
-    test('should contain main title', () => { 
         expect(
             screen.getByRole('heading', { name: /Tic Tac Toe/i })
         ).toBeInTheDocument();
+    });
+
+    describe('modal should', () => { 
+        test('open when user clicks on "restart" button and close if "No, cancel" is pressed', () => { 
+            const { container } = render(
+                <PlayerCoxtext.Provider value={{
+                    player,
+                    setPlayer
+                }}>
+                    <Game />
+                </PlayerCoxtext.Provider>
+            );
+
+            const restartButton = screen.getByRole('button', { name: 'restart' });
+
+            fireEvent.click(restartButton);
+
+            expect(screen.getByText('RESTART GAME?')).toBeInTheDocument();
+
+            const cancelButton = screen.getByRole('button', { name: 'NO, CANCEL' });
+
+            fireEvent.click(cancelButton);
+
+            expect(container).toMatchSnapshot();
+        });
     });
 });
