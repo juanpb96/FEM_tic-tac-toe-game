2c3574f5f9acaf969bbe8e7357ba5f9570dfa875
MasterDev <juanpb.96@gmail.com>
2022-07-04 21:55:52 -0500

    Add click action to restart a game

--- src/components/Game/GameHeader.js
+++ src/components/Game/GameHeader.js
@@ -2,8 +2,16 @@ import { useContext } from 'react';
 import { ASSETS_PATH } from '../../helpers/constants';
 import { PlayerCoxtext } from '../../hocs/PlayerContext';
 
-export const GameHeader = () => {
+export const GameHeader = ({ setModalValues, setShowModal }) => {
     const { player } = useContext(PlayerCoxtext);
+
+    const handleRestart = () => {
+        setModalValues(state => ({
+            ...state,
+            type: 'restart'
+        }));
+        setShowModal(true);
+    };
 
     return (
         <header>
@@ -13,7 +21,7 @@ export const GameHeader = () => {
                 <img src={`${ASSETS_PATH}/icon-turn-${player}.svg`} alt={ player } />
                 <h2>TURN</h2>
             </section>
-            <button aria-label='restart'>
+            <button aria-label='restart' onClick={ handleRestart }>
                 <img src={`${ASSETS_PATH}/icon-restart.svg`} alt='' />
             </button>
         </header>
