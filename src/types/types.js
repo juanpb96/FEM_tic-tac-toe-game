9f089144b1356230137b6b3627ee7e04463f386d
MasterDev <juanpb.96@gmail.com>
2022-07-17 21:10:07 -0500

    Add file to centralize string types

---
+++ src/types/types.js
@@ -0,0 +1,21 @@
+export const MODAL_TYPES = {
+    restart: 'restart',
+    tied: 'tied',
+    player_won: 'player_won',
+    player_lost: 'player_lost',
+    player1_won: 'player1_won',
+    player2_won: 'player2_won',
+};
+
+export const STORAGE = {
+    playerMark: 'PLAYER_MARK',
+    cpuMark: 'CPU_MARK',
+    p1Mark: 'P1_MARK',
+    p2Mark: 'P2_MARK',
+    currentTurnMark: 'CURRENT_TURN_MARK',
+
+    cpuScore: 'CPU_SCORE',
+    playerScore: 'PLAYER_SCORE',
+    p1Score: 'P1_SCORE',
+    p2Score: 'P2_SCORE',
+};
