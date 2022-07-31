75c31525f5faf7cfc5fafb22b5b315a344d32afa
MasterDev <juanpb.96@gmail.com>
2022-07-31 00:09:18 -0500

    Display scores accordingly

--- src/components/Game/GameFooter.js
+++ src/components/Game/GameFooter.js
@@ -1,17 +1,67 @@
+import { STORAGE } from '../../types/types';
+
+const {
+    lsCpuMark,
+    lsCpuScore,
+    lsPlayerMark,
+    lsPlayerScore,
+    lsP1Mark,
+    lsP1Score,
+    lsP2Mark,
+    lsP2Score,
+    lsTiedScore,
+} = STORAGE;
+
 export const GameFooter = () => {
+    const cpuMark = localStorage.getItem(lsCpuMark);
+    const cpuScore = localStorage.getItem(lsCpuScore);
+    const p1Mark = localStorage.getItem(cpuMark ? lsPlayerMark : lsP1Mark);
+    const p1Score = localStorage.getItem(cpuMark ? lsPlayerScore : lsP1Score);
+    const p2Mark = localStorage.getItem(lsP2Mark);
+    const p2Score = localStorage.getItem(lsP2Score);
+    const tiedScore = localStorage.getItem(lsTiedScore);
+
+    let labelPlayerX = '';
+    let labelPlayerO = '';
+    let labelScoreX = 0;
+    let labelScoreO = 0;
+    
+    switch ('X') {
+        case cpuMark:
+            labelPlayerX = 'CPU';
+            labelPlayerO = 'YOU';
+            labelScoreX = cpuScore;
+            labelScoreO = p1Score;
+            break;
+        case p1Mark:
+            labelPlayerX = cpuMark ? 'YOU' : 'P1';
+            labelPlayerO = cpuMark ? 'CPU' : 'P2';
+            labelScoreX = p1Score;
+            labelScoreO = cpuScore;
+            break;
+        case p2Mark:
+            labelPlayerX = 'P2';
+            labelPlayerO = 'P1';
+            labelScoreX = p2Score;
+            labelScoreO = p1Score;
+            break;
+        default:
+            break;
+    }
+
     return (
         <footer>
             <section aria-label='score'>
-                <h2>X</h2>
-                <p>0</p>
+                <h2>X { `(${labelPlayerX})` }</h2>
+                <p>{ labelScoreX }</p>
             </section>
             <section aria-label='score'>
                 <h2>TIES</h2>
-                <p>0</p>
+                <p>{ tiedScore }</p>
             </section>
             <section aria-label='score'>
-                <h2>O</h2>
-                <p>0</p>
+                <h2>O {`(${labelPlayerO})`}</h2>
+                <p>{ labelScoreO }</p>
             </section>
         </footer>
     );
--- src/components/Game/tests/GameFooter.test.js
+++ src/components/Game/tests/GameFooter.test.js
@@ -1,5 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { localStorageMock } from '../../../helpers/mocks/localStorage.mock';
+import { STORAGE } from '../../../types/types';
 import { GameFooter } from '../GameFooter';
+
+const {
+    lsCpuMark,
+    lsCpuScore,
+    lsPlayerMark,
+    lsPlayerScore,
+    lsP1Mark,
+    lsP1Score,
+    lsP2Mark,
+    lsP2Score,
+    lsTiedScore,
+} = STORAGE;
 
 describe('Test <GameFooter />', () => { 
     test('should contain scores', () => { 
@@ -14,4 +28,49 @@ describe('Test <GameFooter />', () => {
             expect(score.children.item(1).tagName).toBe('P');
         });
     });
+
+    describe('should contain titles and scores respectively when game is', () => {
+        afterEach(() => {
+            localStorageMock.clear();
+        });
+
+        test('Player vs CPU', () => {
+            localStorageMock.setItem(lsPlayerMark, 'X');
+            localStorageMock.setItem(lsPlayerScore, '5');
+            localStorageMock.setItem(lsTiedScore, '2');
+            localStorageMock.setItem(lsCpuMark, 'O');
+            localStorageMock.setItem(lsCpuScore, '11');
+    
+            render(<GameFooter />);
+    
+            const scores = screen.getAllByLabelText(/score/i);
+    
+            expect(scores[0].children.item(0).textContent).toBe('X (YOU)');
+            expect(scores[0].children.item(1).textContent).toBe('5');
+            expect(scores[1].children.item(0).textContent).toBe('TIES');
+            expect(scores[1].children.item(1).textContent).toBe('2');
+            expect(scores[2].children.item(0).textContent).toBe('O (CPU)');
+            expect(scores[2].children.item(1).textContent).toBe('11');
+        });
+    
+        test('Player vs Player', () => {
+            localStorageMock.setItem(lsP1Mark, 'O');
+            localStorageMock.setItem(lsP1Score, '11');
+            localStorageMock.setItem(lsTiedScore, '32');
+            localStorageMock.setItem(lsP2Mark, 'X');
+            localStorageMock.setItem(lsP2Score, '14');
+    
+            render(<GameFooter />);
+    
+            const scores = screen.getAllByLabelText(/score/i);
+    
+            expect(scores[0].children.item(0).textContent).toBe('X (P2)');
+            expect(scores[0].children.item(1).textContent).toBe('14');
+            expect(scores[1].children.item(0).textContent).toBe('TIES');
+            expect(scores[1].children.item(1).textContent).toBe('32');
+            expect(scores[2].children.item(0).textContent).toBe('O (P1)');
+            expect(scores[2].children.item(1).textContent).toBe('11');
+        });
+    });
+
 });
