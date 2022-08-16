ce7357b7e44902667b1e1f43d0dfa2527dc21130
MasterDev <juanpb.96@gmail.com>
2022-08-15 22:24:11 -0500

    Initialize values from localStorage, add open modal function and pass it to child components

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,31 +1,104 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
+import { GameContext } from '../../hocs/GameContext';
 import { GameBoard } from './GameBoard';
 import { GameFooter } from './GameFooter';
 import { GameHeader } from './GameHeader';
 import { GameModal } from './GameModal';
+
+import { ACTIONS, STORAGE } from '../../types/types';
 
 const modalInitialState = {
     type: '',
     winnerMark: ''
 };
 
+const {
+    lsBoardState,
+    lsTurnCount,
+    lsCpuMark,
+    lsCurrentTurnMark,
+} = STORAGE;
+
 export const Game = () => {
+    const { gameState, dispatch } = useContext(GameContext);
+
+    const {
+        currentPlayer,
+        board,
+        turnCounter,
+    } = gameState;
+
     const [showModal, setShowModal] = useState(false);
     const [modalValues, setModalValues] = useState(modalInitialState);
+
+    const { type, winnerMark } = modalValues;
+
+    const openModal = (type, winnerMark = '') => {
+        setModalValues({
+            type,
+            winnerMark
+        });
+        setShowModal(true);
+    };
+
+    // TODO: Check if player marks (P1, P2) need to be initialized here too
+    useEffect(() => {
+        if (!board) {
+            const initialBoard = localStorage.getItem(lsBoardState);
+            
+            if (initialBoard) {
+                dispatch({
+                    type: ACTIONS.setGameBoard,
+                    payload: JSON.parse(initialBoard)
+                });
+            }
+        }
+
+        if (!currentPlayer) {
+            const currentTurnMark = localStorage.getItem(lsCurrentTurnMark);
+    
+            if (currentTurnMark) {
+                dispatch({
+                    type: ACTIONS.setCurrentPlayer,
+                    payload: currentTurnMark
+                });
+            }
+        }
+
+        if (!turnCounter) {
+            const turnCounter = +localStorage.getItem(lsTurnCount);
+    
+            if (turnCounter > 0) {
+                dispatch({
+                    type: ACTIONS.setTurnCounter,
+                    payload: turnCounter
+                });
+            }
+        }
+
+        const cpuMark = localStorage.getItem(lsCpuMark);
+        const hasCpuMoved = gameState.board.some(row => row.some(cell => cell === cpuMark));
+
+        console.log({ hasCpuMoved });
+
+        if (hasCpuMoved) {
+            dispatch({
+                type: ACTIONS.setCpuMoveFirst,
+                payload: false
+            });
+        }
+    }, []);
 
     useEffect(() => {
         if (!showModal) {
             setModalValues(modalInitialState);
         }
     }, [showModal]);
-
-    const { type, winnerMark } = modalValues;
     
     return (
         <>
-            <GameHeader
-                setModalValues={ setModalValues }
-                setShowModal={ setShowModal } />
+            <GameHeader openModal={openModal} />
             {
                 showModal &&
                 <GameModal
@@ -34,10 +107,7 @@ export const Game = () => {
                     setShowModal={ setShowModal }
                 />
             }
-            <GameBoard
-                setModalValues={ setModalValues }
-                setShowModal={ setShowModal }
-            />
+            <GameBoard openModal={openModal} />
             <GameFooter />
         </>
     );
