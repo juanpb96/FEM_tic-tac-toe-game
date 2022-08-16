7545c60bbd52b1893cb7afc4e7e02a747677db55
MasterDev <juanpb.96@gmail.com>
2022-08-15 22:17:46 -0500

    Add reducer and actions to handle state effectively

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,21 +1,85 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { PlayerCoxtext } from './hocs/PlayerContext';
-import { AppRouter } from './routers/AppRouter';
-import { STORAGE } from './types/types';
 
-export const TicTacToe = () => {
-    const currentPlayer = localStorage.getItem(STORAGE.lsCurrentTurnMark);
-    const [player, setPlayer] = useState(currentPlayer || 'X');
+import { AppRouter } from './routers/AppRouter';
+import { GameContext } from './hocs/GameContext';
+
+import { ACTIONS } from './types/types';
+import { getEmptyBoard } from './helpers/getEmptyBoard';
+
+const initialState = {
+    currentPlayer: 'X',
+    board: getEmptyBoard(),
+    turnCounter: 1,
+    isGameOver: false,
+    isCpuFirstMove: true
+};
+
+const gameReducer = (state, action) => {
+    switch (action.type) {
+        case ACTIONS.setChangeTurn:
+            return {
+                ...state,
+                board: [...action.payload],
+                currentPlayer: state.currentPlayer === 'X' ? 'O' : 'X',
+                turnCounter: state.turnCounter + 1,
+            };
+        case ACTIONS.setCurrentPlayer:
+            return {
+                ...state,
+                currentPlayer: action.payload
+            };
+        case ACTIONS.setGameBoard:
+            return {
+                ...state,
+                board: [...action.payload]
+            };
+        case ACTIONS.resetGameBoard:
+            return {
+                ...state,
+                board: getEmptyBoard()
+            };
+        case ACTIONS.setTurnCounter:
+            return {
+                ...state,
+                turnCounter: action.payload
+            };
+        case ACTIONS.resetTurnCounter:
+            return {
+                ...state,
+                turnCounter: 1
+            };
+        case ACTIONS.setGameOver:
+            return {
+                ...state,
+                isGameOver: action.payload
+            };
+        case ACTIONS.setCpuMoveFirst:
+            return {
+                ...state,
+                isCpuFirstMove: action.payload
+            };
+        case ACTIONS.resetGame:
+            return {
+                ...state,
+                currentPlayer: 'X',
+                board: getEmptyBoard(),
+                turnCounter: 1,
+                isGameOver: false,
+            };
+        default:
+            return state;
+    }
+};
+
+export const TicTacToe = () => {  
+    const [gameState, dispatch] = useReducer(gameReducer, initialState);    
 
     return (
-        <PlayerCoxtext.Provider value={{
-            player,
-            setPlayer
-        }}>
+        <GameContext.Provider value={{ gameState, dispatch }}>
             <BrowserRouter>
                 <AppRouter />
             </BrowserRouter>
-        </PlayerCoxtext.Provider>
+        </GameContext.Provider>
     );
 };
