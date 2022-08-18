303133e7a8f6a6edee0ef41ec198d80860592471
MasterDev <juanpb.96@gmail.com>
2022-08-17 22:30:17 -0500

    Add styles for New Game screen and replace images with SVGs

--- src/components/NewGame/NewGame.js
+++ src/components/NewGame/NewGame.js
@@ -74,47 +74,47 @@ export const NewGame = () => {
             <h1 className="sr-only">Tic tac toe</h1>
             <img className='logo' src={`${ASSETS_PATH}/logo.svg`} alt=''/>
 
-            <section className='[ pick-player ]' role='radiogroup' aria-labelledby='mark-label'>
-                <h2 id='mark-label'>PICK PLAYER 1’S MARK</h2>
-                <div>
+            <section className='selection-container' role='radiogroup' aria-labelledby='mark-label'>
+                <h2 id='mark-label' className='[ fs-4 color-silver letter-s mb-6 ]'>PICK PLAYER 1’S MARK</h2>
+                <div className='[ flex bg-dark-navy mb-4 px-2 py-2.5 br-2.5 ]'>
                     <div 
                         role='radio' 
                         aria-checked={isMarkXChecked} 
                         tabIndex='0'
+                        className='[ mark ][ flex flex-1 flex-center py-3 br-2.5 ]'
                         onClick={() => handleMarkChange('X')}
                     >
-                        {
-                            isMarkXChecked
-                            ? <img src={`${ASSETS_PATH}/icon-x.svg`} alt='X'/>  
-                            : <img src={`${ASSETS_PATH}/icon-x-outline.svg`} alt='X'/>
-                        }
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M31.5569 5.28973L26.7103 0.443061C26.1195 -0.147687 25.1617 -0.147687 24.571 0.443061L16 9.01404L7.42902 0.443061C6.83827 -0.147687 5.88048 -0.147687 5.28973 0.443061L0.443061 5.28973C-0.147687 5.88048 -0.147687 6.83827 0.443061 7.42902L9.01404 16L0.443061 24.571C-0.147687 25.1617 -0.147687 26.1195 0.443061 26.7103L5.28973 31.5569C5.88048 32.1477 6.83827 32.1477 7.42902 31.5569L16 22.986L24.571 31.5569C25.1617 32.1477 26.1195 32.1477 26.7103 31.5569L31.5569 26.7103C32.1477 26.1195 32.1477 25.1617 31.5569 24.571L22.986 16L31.5569 7.42902C32.1477 6.83827 32.1477 5.88048 31.5569 5.28973Z" fill=""/>
+                        </svg>
                     </div>
                     <div 
                         role='radio' 
                         aria-checked={!isMarkXChecked} 
                         tabIndex='-1'
+                        className='[ mark ][ flex flex-1 flex-center py-3 br-2.5 ]'
                         onClick={() => handleMarkChange('O')}
                     >
-                        {
-                            !isMarkXChecked
-                            ? <img src={`${ASSETS_PATH}/icon-o.svg`} alt='O'/>
-                            : <img src={`${ASSETS_PATH}/icon-o-outline.svg`} alt='O'/>
-                        }
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M31.7412 15.8706C31.7412 7.10551 24.6357 0 15.8706 0C7.10551 0 0 7.10551 0 15.8706C0 24.6357 7.10551 31.7412 15.8706 31.7412C24.6357 31.7412 31.7412 24.6357 31.7412 15.8706ZM9.4048 15.8706C9.4048 12.2996 12.2996 9.4048 15.8706 9.4048C19.4416 9.4048 22.3364 12.2996 22.3364 15.8706C22.3364 19.4416 19.4416 22.3364 15.8706 22.3364C12.2996 22.3364 9.4048 19.4416 9.4048 15.8706Z" fill=""/>
+                        </svg>
                     </div>
                 </div>
-                <p>REMEMBER : X GOES FIRST</p>
+                <p className='[ fs-3.5 color-silver opacity ]'>REMEMBER : X GOES FIRST</p>
             </section>
 
-            <button 
-                className='[ button ][ yellow ]'
-                data-type='primary'
-                onClick={ () => handleNewGame(true) }>New Game (VS CPU)
-            </button>
-            <button 
-                className='[ button ][ blue ]'
-                data-type='primary'
-                onClick={ () => handleNewGame(false) }>New Game (VS Player)
-            </button>
+            <div className='[ flex flex-col gap-1 ]'>
+                <button 
+                    className='[ button ][ yellow ]'
+                    data-type='primary'
+                    onClick={ () => handleNewGame(true) }>New Game (VS CPU)
+                </button>
+                <button 
+                    className='[ button ][ blue ]'
+                    data-type='primary'
+                    onClick={ () => handleNewGame(false) }>New Game (VS Player)
+                </button>
+            </div>
         </main>
     );
 };
