1b9223c1a9b2e6adb3b2e360731a28eb08be2ad1
MasterDev <juanpb.96@gmail.com>
2022-08-20 15:30:33 -0500

    Add styles for game header

--- src/components/Game/GameHeader.js
+++ src/components/Game/GameHeader.js
@@ -13,14 +13,22 @@ export const GameHeader = ({ openModal }) => {
     };
 
     return (
-        <header>
-            <h1>Tic tac toe</h1>
+        <header className='flex space-between align-center mb-16'>
+            <h1 className='sr-only'>Tic tac toe</h1>
             <img src={`${ASSETS_PATH}/logo.svg`} alt='' />
-            <section>
+            <section
+                className='[ turn ][ flex gap-2.5 box-navy br-1.5 ]'
+                data-type='secondary'
+            >
                 <img src={`${ASSETS_PATH}/icon-turn-${currentPlayer}.svg`} alt={ currentPlayer } />
-                <h2>TURN</h2>
+                <h2 className='fs-3.5 color-silver'>TURN</h2>
             </section>
-            <button aria-label='restart' onClick={ handleRestart }>
+            <button 
+                aria-label='restart'
+                className='[ button-restart ][ br-1.5 ]'
+                data-type='secondary'
+                onClick={ handleRestart }
+            >
                 <img src={`${ASSETS_PATH}/icon-restart.svg`} alt='' />
             </button>
         </header>
