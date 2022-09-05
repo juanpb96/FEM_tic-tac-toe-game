e274de10dc8624b881746096cf9ecb29bc95785b
MasterDev <juanpb.96@gmail.com>
2022-09-05 01:08:02 -0500

    Remove code that needs to be handled by parent component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,9 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { GameContext } from '../../hocs/GameContext';
 import { GameBoard } from './GameBoard';
 import { GameFooter } from './GameFooter';
 import { GameHeader } from './GameHeader';
 import { GameModal } from './GameModal';
-
-import { ACTIONS, STORAGE } from '../../types/types';
 
 import './game.scss';
 
@@ -15,22 +12,7 @@ const modalInitialState = {
     winnerMark: ''
 };
 
-const {
-    lsBoardState,
-    lsTurnCount,
-    lsCpuMark,
-    lsCurrentTurnMark,
-} = STORAGE;
-
 export const Game = () => {
-    const { gameState, dispatch } = useContext(GameContext);
-
-    const {
-        currentPlayer,
-        board,
-        turnCounter,
-    } = gameState;
-
     const [showModal, setShowModal] = useState(false);
     const [modalValues, setModalValues] = useState(modalInitialState);
 
@@ -43,52 +25,6 @@ export const Game = () => {
         });
         setShowModal(true);
     };
-
-    // TODO: Check if player marks (P1, P2) need to be initialized here too
-    useEffect(() => {
-        if (!board) {
-            const initialBoard = localStorage.getItem(lsBoardState);
-            
-            if (initialBoard) {
-                dispatch({
-                    type: ACTIONS.setGameBoard,
-                    payload: JSON.parse(initialBoard)
-                });
-            }
-        }
-
-        if (!currentPlayer) {
-            const currentTurnMark = localStorage.getItem(lsCurrentTurnMark);
-    
-            if (currentTurnMark) {
-                dispatch({
-                    type: ACTIONS.setCurrentPlayer,
-                    payload: currentTurnMark
-                });
-            }
-        }
-
-        if (!turnCounter) {
-            const turnCounter = +localStorage.getItem(lsTurnCount);
-    
-            if (turnCounter > 0) {
-                dispatch({
-                    type: ACTIONS.setTurnCounter,
-                    payload: turnCounter
-                });
-            }
-        }
-
-        const cpuMark = localStorage.getItem(lsCpuMark);
-        const hasCpuMoved = gameState.board.some(row => row.some(cell => cell === cpuMark));
-
-        if (hasCpuMoved) {
-            dispatch({
-                type: ACTIONS.setCpuMoveFirst,
-                payload: false
-            });
-        }
-    }, []);
 
     useEffect(() => {
         if (!showModal) {
